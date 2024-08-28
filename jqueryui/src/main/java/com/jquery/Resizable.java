a6e65928b17c2a209be3c6545d8910f0b49ed27b
kayatk <thillaikayatk@gmail.com>
2024-08-28 09:08:47 +0200

    Resizable script

---
+++ jqueryui/src/main/java/com/jquery/Resizable.java
@@ -0,0 +1,13 @@
+package com.jquery;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Resizable {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window();
+        driver.get("https://jqueryui.com/resizable/");
+        driver.switchTo().frame(0);
+    }
+}
