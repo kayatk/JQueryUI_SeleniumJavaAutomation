ed6be59ebe016e02147be966cbd81148376f82a2
kayatk <thillaikayatk@gmail.com>
2024-08-30 22:23:00 +0200

    Resizable script

--- jqueryui/src/main/java/com/jquery/Resizable.java
+++ jqueryui/src/main/java/com/jquery/Resizable.java
@@ -1,14 +1,27 @@
 package com.jquery;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class Resizable {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
-        driver.manage().window();
+        driver.manage().window().maximize();
         driver.get("https://jqueryui.com/resizable/");
         driver.switchTo().frame(0);
+
+        WebElement element=driver.findElement(By.xpath("//div[contains(@class,' ui-icon-gripsmall-diagonal-se')]"));
+       
+        Actions act = new Actions(driver);
+        act.clickAndHold(element)
+        .moveByOffset(100, 100)
+        .release(element)
+        .build();
+        act.perform();
+       // act.dragAndDropBy(element,200,200).build().perform();
 
         driver.quit();
     }
